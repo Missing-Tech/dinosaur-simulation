09cd314a35c6760e1390389bf192fed71c130d06
Joseph Grabski <josephgrabski31@gmail.com>
2022-03-01 16:32:23 +0000

    Comments

--- Brontosaurus.java
+++ Brontosaurus.java
@@ -1,30 +1,27 @@
-import java.util.List;
-import java.util.Random;
-
 /**
- * A simple model of a rabbit.
- * Rabbits age, move, breed, and die.
+ * A simple model of a Brontosaurus.
+ * They age, move, breed, and die.
  * 
- * @author David J. Barnes and Michael Kölling
- * @version 2016.02.29 (2)
+ * @author David J. Barnes and Michael Kölling and Joseph Grabski and Yukesh Shrestha
+ * @version 2022.03.01 (3)
  */
 public class Brontosaurus extends Prey {
-    // Characteristics shared by all rabbits (class variables).
+    // Characteristics shared by all Brontosauruss (class variables).
 
-    // The age at which a rabbit can start to breed.
+    // The age at which a Brontosaurus can start to breed.
     private static final int BREEDING_AGE = 35;
-    // The age to which a rabbit can live.
+    // The age to which a Brontosaurus can live.
     private static final int MAX_AGE = 80;
-    // The likelihood of a rabbit breeding.
+    // The likelihood of a Brontosaurus breeding.
     private static final double BREEDING_PROBABILITY = 0.07;
     // The maximum number of births.
     private static final int MAX_LITTER_SIZE = 2;
 
     /**
-     * Create a new rabbit. A rabbit may be created with age
+     * Create a new Brontosaurus. A Brontosaurus may be created with age
      * zero (a new born) or with a random age.
      * 
-     * @param randomAge If true, the rabbit will have a random age.
+     * @param randomAge If true, the Brontosaurus will have a random age.
      * @param field     The field currently occupied.
      * @param location  The location within the field.
      */
